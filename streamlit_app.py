ee4eda54309ad19e677971ebd84a78c1530d3798
padhysunita <140104715+padhysunita@users.noreply.github.com>
2023-07-24 17:23:37 +0530

    Update streamlit_app.py
    added mom's favourite breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,11 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+
+streamlit.title('My Mom's New Helthy Dinner')
+
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamli.text('🥑🍞 Avocado Toast')
