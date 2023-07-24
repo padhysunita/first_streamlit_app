f0a372a2b418aeed95cf0f233a6f1085019ddeb9
padhysunita <140104715+padhysunita@users.noreply.github.com>
2023-07-24 17:24:57 +0530

    Update streamlit_app.py
    corrected syntax issue

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
-streamlit.title('My Mom's New Helthy Dinner')
+streamlit.title('My Moms New Helthy Dinner')
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
